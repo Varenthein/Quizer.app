826bd3165352e14f2f229d34fd99d0c89e8e8df1
Paweł Zagrobelny <raventhein@gmail.com>
2017-02-14 10:12:24 +0100

    Added comments

--- app.js
+++ app.js
@@ -30,13 +30,14 @@ app.controller('QuizCreatorController', function($scope) { //create Quizer contr
       },
     ];
 
-    //Add
+    //Add question
     $scope.addQuestion = () => {
 
         $scope.Quests.push({ text: '', image: '', answers: [], active: false });
 
     };
 
+    //Remove question
     $scope.removeQuestion = (item) => {
 
         let index = $scope.Quests.indexOf(item);
@@ -44,7 +45,7 @@ app.controller('QuizCreatorController', function($scope) { //create Quizer contr
 
     };
 
-
+    //Add answer
     $scope.addAnswer = (item) => {
 
         let index = $scope.Quests.indexOf(item);
@@ -52,6 +53,7 @@ app.controller('QuizCreatorController', function($scope) { //create Quizer contr
 
     };
 
+    //Remove answer
     $scope.removeAnswer = (question, item) => {
 
         let quest_index = $scope.Quests.indexOf(question);
@@ -60,37 +62,38 @@ app.controller('QuizCreatorController', function($scope) { //create Quizer contr
 
     };
 
+    //generate quiz
     $scope.generate = () => {
 
         let errors = $scope.Quests.reduce((sum, quest) => {
 
               let errors = 0;
-              if(quest.title == "") errors++;
-              if(quest.answers.length <= 0) errors++;
-              if(quest.answers.filter((item) => { return item.valid === true }).length <= 0) errors++;
-              quest.answers.reduce((sum, answer) => { if(answer.text == "") errors++; },0);
+              if(quest.title == "") errors++; //if there is a question without title, it won't generate emebd code
+              if(quest.answers.length <= 0) errors++; //the same if there is a question without answer
+              if(quest.answers.filter((item) => { return item.valid === true }).length <= 0) errors++; //return error if there is a question without valid answers
+              quest.answers.reduce((sum, answer) => { if(answer.text == "") errors++; },0); //return error if there is an answer without text
               return sum + errors;
 
         },0);
 
-        if($scope.quiz.title == '') $scope.quiz.title = 'Unnamed';
-        if($scope.quiz.author == '') $scope.quiz.author = 'Unknown';
+        if($scope.quiz.title == '') $scope.quiz.title = 'Unnamed'; //set default title if not set
+        if($scope.quiz.author == '') $scope.quiz.author = 'Unknown'; //set default author if not set
 
-        if(errors == 0) {
+        if(errors == 0) { //if there are no errors
 
-              let codeBox = document.createElement("textarea")
+              let codeBox = document.createElement("textarea"); //create textarea to store embed code
               codeBox.id = "codeBox";
-              codeBox.className = "form-control";
+              codeBox.className = "form-control"; //add bootstrap styling
               jQuery(codeBox).text(`<!-- Quizer app block -->\n<div id="quizer-app-block"></div>\n<script>var quizer_questions = ${JSON.stringify($scope.Quests)}\nvar quiz_info = ${JSON.stringify($scope.quiz)};</script>\n<link href="http://sofasport.pl/Tools/QuizMaker/styles/default.min.css" rel="stylesheet"/>\n<script src="http://sofasport.pl/Tools/QuizMaker/quizLoader.min.js"></script>\n<!-- Quizer app block end -->`);
-              jQuery('#generateModal .modal-body p').html('You can embed this code on your website: ').parent().append(jQuery(codeBox));
-              jQuery('#generateModal').modal();
+              jQuery('#generateModal .modal-body p').html('You can embed this code on your website: ').parent().append(jQuery(codeBox)); //set modal text
+              jQuery('#generateModal').modal(); //show modal
 
         }
-        else {
+        else { //if there were some errors
 
-              jQuery('#generateModal textarea').remove();
-              jQuery('#generateModal .modal-body p').html(`There were ${errors} errors`);
-              jQuery('#generateModal').modal();
+              jQuery('#generateModal textarea').remove(); //remove textarea
+              jQuery('#generateModal .modal-body p').html(`There were ${errors} errors`); //set modal text
+              jQuery('#generateModal').modal(); //show modal
 
         }
 
